807d6a76c80eda5385e80d411518bfc02006d247
narayank97 <narayankarun@gmail.com>
2020-04-26 18:36:15 -0700

    login system so far

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const bcrypt = require("bcrypt");
+
+const users = []; // my db rn
+app.set('view-engine','ejs');
+app.use(express.urlencoded({extended:false}));
+
+app.get('/',(req,res) => {
+    res.render('index.ejs',{ name: 'Karun' });
+});
+
+app.get('/login',(req,res)=>{
+    res.render('login.ejs')
+})
+
+app.post('/login',(req,res)=>{
+    
+})
+
+app.get('/register',(req,res)=>{
+    res.render('register.ejs')
+})
+
+app.post('register',(req,res)=>{
+    
+})
+
+app.listen(3000);
